cad4d05c16c147909645d3d91acbe84815c14611
Suryakant Gupta <suryakant@simformsolutions.com>
2024-06-19 17:01:24 +0530

    serversession fix

--- src/app/server/page.jsx
+++ src/app/server/page.jsx
@@ -5,7 +5,7 @@ import UserCard from "../components/UserCard"
 import { redirect } from "next/navigation"
 
 export default async function ServerPage() {
-    const session = await getServerSession(handler)
+    const session = await getServerSession(options)
     console.log('servercheck', { session })
     // if (!session) {
     //     redirect('/api/auth/signin?callbackUrl=/server')
